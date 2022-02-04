c942c737e7243bd37ee8123644e2198965b4c502
Krzysztof Kobiałka <krzysztof.kobialka90@gmail.com>
2022-02-04 11:20:50 +0100

    defining database in settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_tutorial',
+        'USER': 'postgres',
+        'PASSWORD': 'test_postgres',
+        'HOST': '127.0.0.1'
     }
 }
 
